fab471c9ee9949b3992e3e14e19aef00f8a7b39a
Jocelyn-Chen5 <cjl1759648974@outlook.com>
2024-04-19 17:00:53 +0100

    complete comments for bidirectional grpc

--- grpc-client/src/main/java/SecurityClient.java
+++ grpc-client/src/main/java/SecurityClient.java
@@ -12,21 +12,27 @@ public class SecurityClient {
     public static void main(String[] args) {
         ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost",8060).usePlaintext().build();
         try{
+            // Create the agency to invoke the grpc method
             HelloSecurityGrpc.HelloSecurityStub securityStub = HelloSecurityGrpc.newStub(managedChannel);
+            // The agency stub invokes the grpc method
             securityRequestStreamObserver = securityStub.securityService(new StreamObserver<HelloSecurityProto.SecurityResponse>() {
                 @Override
                 public void onNext(HelloSecurityProto.SecurityResponse securityResponse){
+                    // The client sends the command as it accepts danger warning info from the server
                     if (!securityResponse.getWarnings().isEmpty()) {
                         System.out.println(securityResponse.getWarnings());
                         securityRequestStreamObserver.onNext(HelloSecurityProto.SecurityRequest.newBuilder().setOnInstructs("Turn on the camera and alarm").build());
                     }
+                    // The client accepts data from the server as it completes operation successfully
                     if (!securityResponse.getOperations().isEmpty()) {
                         System.out.println(securityResponse.getOperations());
                     }
+                    // The client accepts danger lift tips from the server and sends another command to the server
                     if (!securityResponse.getDangerRemove().isEmpty()) {
                         System.out.println(securityResponse.getDangerRemove());
                         securityRequestStreamObserver.onNext(HelloSecurityProto.SecurityRequest.newBuilder().setOffInstructs("Turn off the camera and alarm").build());
                     }
+                    // The client accepts the successful operations from the server and client requests end
                     if (!securityResponse.getOperationsTwo().isEmpty()) {
                         System.out.println(securityResponse.getOperationsTwo());
                         securityRequestStreamObserver.onCompleted();
--- grpc-server/src/main/java/com/security/SecurityServiceImpl.java
+++ grpc-server/src/main/java/com/security/SecurityServiceImpl.java
@@ -5,15 +5,18 @@ import io.grpc.stub.StreamObserver;
 public class SecurityServiceImpl extends HelloSecurityGrpc.HelloSecurityImplBase{
     @Override
     public StreamObserver<HelloSecurityProto.SecurityRequest> securityService(StreamObserver<HelloSecurityProto.SecurityResponse> responseObserver) {
+        // The server sends the security warning to the client as it detects some danger
         responseObserver.onNext(HelloSecurityProto.SecurityResponse.newBuilder().setWarnings("Detecting an unidentified person entering").build());
         return new StreamObserver<HelloSecurityProto.SecurityRequest>() {
             @Override
             public void onNext(HelloSecurityProto.SecurityRequest securityRequest) {
+                // The server write commands sent to the client as it accepts info "Turn on the camera and alarm" from the client
                 if (securityRequest.getOnInstructs().equals("Turn on the camera and alarm")) {
                     System.out.println(securityRequest.getOnInstructs());
                     responseObserver.onNext(HelloSecurityProto.SecurityResponse.newBuilder().setOperations("Turn on the camera and alarm successfully").build());
                     responseObserver.onNext(HelloSecurityProto.SecurityResponse.newBuilder().setDangerRemove("The unknown person has already left").build());
                 }
+                // The server write operation info "Turn off the camera and alarm successfully" back to the client as it accepts "Turn off" command
                 if (securityRequest.getOffInstructs().equals("Turn off the camera and alarm")) {
                     System.out.println(securityRequest.getOffInstructs());
                     responseObserver.onNext(HelloSecurityProto.SecurityResponse.newBuilder().setOperationsTwo("Turn off the camera and alarm successfully").build());
